57840f8d44eff3ce38c03c8add000b9b4f475a56
Michelle Glass <mglass@Michelles-MacBook-Air.local>
2020-07-07 12:02:55 -0500

    set up

---
+++ app.ts
@@ -0,0 +1,5 @@
+const myName: string = 'Michelle Glass'; 
+const myField: string = 'Boot-camp Student and Intern for SHS'; 
+const myDescription: string = 'An artistic, excitable Disney princess in disguise; trying to learn to code, with the help of her companion, the coding corgi.';
+let interests: string[] = ['Painting', 'Sewing -mostly quilting', 'Crafts', 'Disney', 'Corgis', 'Cats', 'Animals in general', 'Coding', 'Cooking' ];
+
